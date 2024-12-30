f828324a57272fa246f9f5f31c2210cc16a74f0c
dengw <dengw@ion.ac.cn>
2024-12-30 09:51:25 +0800

    interface setting completed

--- dw-api/app.js
+++ dw-api/app.js
@@ -6,6 +6,10 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const adminArticlesRouter = require('./routes/admin/articles');
+const adminCategoriesRouter = require('./routes/admin/categories');
+const adminSettingsRouter = require('./routes/admin/settings');
+
+
 
 const app = express();
 
@@ -18,5 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/articles', adminArticlesRouter);
+app.use('/admin/categories', adminCategoriesRouter);
+app.use('/admin/settings', adminSettingsRouter);
 
 module.exports = app;
